ebfbc3acb5d89caf76ef215391e8a02b6e3ddb74
Andrea Pavoni <andrea.pavoni@gmail.com>
2018-04-11 11:49:42 +0200

    bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugEtsCache.Mixfile do
   def project do
     [
       app: :plug_ets_cache,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
