1eaa8afa5915c15312224b23ca58e63442123bf0
Jerod Santo <jerod@changelog.com>
2018-08-09 08:35:54 -0500

    Minor refactor of `cache_response` argument matching

--- lib/plug_ets_cache/response.ex
+++ lib/plug_ets_cache/response.ex
@@ -15,16 +15,13 @@ defmodule PlugEtsCache.Response do
       end
   """
 
-  def cache_response(%Plug.Conn{resp_body: nil, state: :unset} = conn), do: conn
+  def cache_response(%Plug.Conn{state: :unset} = conn), do: conn
 
-  def cache_response(%Plug.Conn{resp_body: body, state: :sent} = conn) do
-    cache_response(conn, nil)
-  end
+  def cache_response(%Plug.Conn{state: :sent} = conn), do: cache_response(conn, nil)
 
-  def cache_response(%Plug.Conn{resp_body: nil, state: :unset} = conn, _ttl), do: conn
+  def cache_response(%Plug.Conn{state: :unset} = conn, _ttl), do: conn
 
-  def cache_response(%Plug.Conn{resp_body: body, state: :sent, status: status} = conn, ttl)
-      when status in 200..299 do
+  def cache_response(%Plug.Conn{state: :sent, resp_body: body, status: status} = conn, ttl) when status in 200..299 do
     content_type =
       conn
       |> Plug.Conn.get_resp_header("content-type")
